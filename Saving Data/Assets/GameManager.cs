a8ddd325cca802230545d8f7cf38230a0ad66d12
Brent Gilson <elwais12@gmail.com>
2018-10-05 13:11:52 -0700

    testing out the entire project

---
+++ Saving Data/Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour 
+{
+
+	public int health;
+	public Text healthText;
+
+	// Use this for initialization
+	void Start ()
+	{
+		DontDestroyOnLoad (gameObject);
+
+		//health = 100;
+		PlayerPrefs.GetInt("health");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		healthText.text = health.ToString();
+
+		if (Input.GetKey (KeyCode.Q))
+			health--;
+		if (Input.GetKey (KeyCode.E))
+			health++;
+	}
+}
---
+++ Saving Data/Assets/Save.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Save : MonoBehaviour {
+
+	GameObject es;
+
+	// Use this for initialization
+	void Start () {
+		es = GameObject.Find("EventSystem");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter(Collider p)
+	{
+		if (p.gameObject.tag == "Player") 
+		{
+			//save game
+			PlayerPrefs.SetInt("health", es.GetComponent<GameManager>().health);
+		}
+	}
+
+
+}
